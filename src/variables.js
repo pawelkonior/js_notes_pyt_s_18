d52088c572a5792440d6efb2dadc75e434a0a390
Pawel Konior <pawel.konior@gmail.com>
2024-05-13 11:32:17 +0200

    js variables rules

--- src/variables.js
+++ src/variables.js
@@ -1,1 +1,69 @@
-console.log('elo');
+// re-deklaracja
+// var x = 42;
+// let x1 = 42;
+// const x2 = 42;
+//
+// var x = 10; // można re deklarować, ale tylko quirks mode
+// let x1 = 10; // nie można
+// const x2 = 10; // nie można
+
+
+// re-inicjalizacja
+// var x = 42;
+// let x1 = 42;
+// const x2 = 42;
+//
+// x = 10;
+// x1 = 10;
+// x2 = 10;
+
+// hoisting
+// przenoszenie deklaracji zmiennych, funkcji, klas na górę
+// aktualnie przetwarzanego zasięgu
+
+// console.log(x);
+// var x = 42;
+//
+// var x_;
+// console.log(x_)
+// x_ = 42;
+
+// (TDZ = temporary dead zone)
+// console.log(x);
+// let x = 42;
+
+// Scope
+// widoczność, dostępność identyfikatorów
+// block, function
+
+// if (1){
+//     var x = 42;
+//     let x1 = 42;
+//     const x2 = 42;
+// }
+//
+// console.log(x);
+// console.log(x1);
+// console.log(x2);
+
+
+// let x = 42;
+//
+// function magic(){
+//     x = x + 10;
+//     // let x = 10;
+//     console.log(x);
+// }
+//
+// console.log(x);
+// magic();
+// console.log(x);
+
+// const x = [1, 2, 3];
+//
+// function magic(collection, value){
+//     collection.push(value);
+// }
+//
+// magic(x, 42);
+// console.log(x);
